fb42dc67de586bdac0da7bbad00d914dee81b861
chincharovpc <chincharovpc@yandex.ru>
2024-02-16 00:04:11 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import {ref, watch} from "vue";
+import Ru from "./components/ru.vue";
+import En from "./components/en.vue";
+import Bg from "./components/bg.vue";
+
+const selected = ref(0);
+
+watch(() => selected.value, (id: typeof selected.value) => {
+  switch (id) {
+    case 1: {
+      console.log("Нажата кнопка 1")
+      break;
+    }
+    case 2: {
+      console.log("Нажата кнопка 2")
+      break;
+    }
+  }
+})
+
+const onClickContainer = () => {
+  selected.value = 0
+}
+
+const onClickBlock = (id: number) => {
+  if (selected.value === id) {
+    selected.value = 0
+  } else {
+    selected.value = id
+  }
+}
+</script>
+
+<template>
+  <div class="container" @click="onClickContainer">
+    <div class="blocks">
+      <div class="block" :class="selected == 1 ? 'selected' : ''" @click.stop="onClickBlock(1)">
+        <Bg class="bg"/>
+        <Ru class="text"/>
+      </div>
+      <div class="block" :class="selected == 2 ? 'selected' : ''" @click.stop="onClickBlock(2)">
+        <Bg class="bg"/>
+        <En class="text"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/bg.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <svg width="1290" height="758" viewBox="0 0 1290 758" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g filter="url(#filter0_d_2124_172)">
+      <path d="M97 97V657H1043.88L1189 543.672V97H97Z" fill="#A9007C"/>
+    </g>
+    <defs>
+      <filter id="filter0_d_2124_172" x="0.599998" y="0.599998" width="1288.8" height="756.8" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+        <feOffset dx="2" dy="2"/>
+        <feGaussianBlur stdDeviation="49.2"/>
+        <feComposite in2="hardAlpha" operator="out"/>
+        <feColorMatrix type="matrix" values="0 0 0 0 0.557387 0 0 0 0 0.711693 0 0 0 0 1 0 0 0 1 0"/>
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2124_172"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2124_172" result="shape"/>
+      </filter>
+    </defs>
+  </svg>
+
+</template>
+
+<style scoped>
+
+</style>
