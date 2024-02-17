3175e52d297064962dea2a4fddd5a8ec05387760
initum <initum@yandex.ru>
2024-02-17 14:42:48 +0300

    ADD 3 BUTTONS

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import {onMounted, ref, watch} from "vue";
-import type {Ref} from "vue";
+import { onMounted, ref, watch } from "vue";
+import axios from "axios";
+import Uznai from "./components/uznai.vue";
 import Ru from "./components/ru.vue";
 import En from "./components/en.vue";
 import Bg from "./components/bg.vue";
@@ -8,45 +9,66 @@ import ElectraRu from "./assets/Electra - TECHNIC[RU].wav";
 import ElectraEn from "./assets/Electra - TECHNIC [ENG].wav";
 
 const selected = ref(0);
-const audio:Ref<HTMLAudioElement[]> = ref([new Audio(ElectraRu), new Audio(ElectraEn)]);
+const audio = ref([new Audio(ElectraRu), new Audio(ElectraEn)]);
 
-onMounted(()=>{
-  audio.value.forEach(a=>{
-    a.onended = ()=>onClickBlock(0)
-  })
-})
+onMounted(() => {
+  audio.value.forEach(a => {
+    a.onended = () => onClickBlock(0);
+  });
+});
 
-watch(() => selected.value, (id: typeof selected.value) => {
-  audio.value.forEach(a=>{
-    a.pause()
-    a.currentTime = 0
-  })
+const sendCommandToVMix = (functionName, inputNumber) => {
+  // Замените адрес на актуальный адрес вашего vMix HTTP API
+  const vmixApiUrl = `http://10.30.40.65:8088/api`;
+
+  axios.get(`${vmixApiUrl}?Function=${functionName}&Input=${inputNumber}`)
+      .then(response => {
+        console.log('vMix command response:', response.data);
+      })
+      .catch(error => {
+        console.error('Error sending command to vMix:', error);
+      });
+};
+
+watch(() => selected.value, (id) => {
+  audio.value.forEach(a => {
+    a.pause();
+    a.currentTime = 0;
+  });
 
   switch (id) {
-    case 1: {
-      console.log("Нажата кнопка 1")
-      audio.value[0].play()
+    case 1:
+      console.log("Нажата кнопка 1");
+      // Отправка HTTP запроса в vMix
+      sendCommandToVMix('Cut', '3');
       break;
-    }
-    case 2: {
-      console.log("Нажата кнопка 2")
-      audio.value[1].play()
+    case 2:
+      console.log("Нажата кнопка 2");
+      // Отправка HTTP запроса в vMix
+      sendCommandToVMix('Cut', '2');
       break;
-    }
+    case 3:
+      console.log("Нажата кнопка 3");
+      // audio.value[1].play();
+      // Отправка HTTP запроса в vMix
+      sendCommandToVMix('Cut', '1');
+      break;
   }
-})
+});
 
 const onClickContainer = () => {
-  selected.value = 0
-}
+  selected.value = 0;
+  sendCommandToVMix('Cut', '1');
+};
 
 const onClickBlock = (id: number) => {
   if (selected.value === id) {
-    selected.value = 0
+    selected.value = 0;
   } else {
-    selected.value = id
+    selected.value = id;
   }
-}
+};
+document.addEventListener('contextmenu', event => event.preventDefault());
 </script>
 
 <template>
@@ -54,9 +76,13 @@ const onClickBlock = (id: number) => {
     <div class="blocks">
       <div class="block" :class="selected == 1 ? 'selected' : ''" @click.stop="onClickBlock(1)">
         <Bg class="bg"/>
-        <Ru class="text"/>
+        <Uznai class="text"/>
       </div>
       <div class="block" :class="selected == 2 ? 'selected' : ''" @click.stop="onClickBlock(2)">
+        <Bg class="bg"/>
+        <Ru class="text"/>
+      </div>
+      <div class="block" :class="selected == 3 ? 'selected' : ''" @click.stop="onClickBlock(3)">
         <Bg class="bg"/>
         <En class="text"/>
       </div>
--- src/components/bg.vue
+++ src/components/bg.vue
@@ -3,24 +3,23 @@
 </script>
 
 <template>
-  <svg width="1290" height="758" viewBox="0 0 1290 758" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g filter="url(#filter0_d_2124_172)">
-      <path d="M97 97V657H1043.88L1189 543.672V97H97Z" fill="#A9007C"/>
+  <svg width="1474" height="572" viewBox="0 0 1474 572" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g filter="url(#filter0_d_2208_1530)">
+      <path d="M97 96.983V470.983H1240.06L1373 367.351V96.983H97Z" fill="#A9007C"/>
     </g>
     <defs>
-      <filter id="filter0_d_2124_172" x="0.599998" y="0.599998" width="1288.8" height="756.8" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+      <filter id="filter0_d_2208_1530" x="0.599998" y="0.58297" width="1472.8" height="570.8" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
         <feFlood flood-opacity="0" result="BackgroundImageFix"/>
         <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
         <feOffset dx="2" dy="2"/>
         <feGaussianBlur stdDeviation="49.2"/>
         <feComposite in2="hardAlpha" operator="out"/>
         <feColorMatrix type="matrix" values="0 0 0 0 0.557387 0 0 0 0 0.711693 0 0 0 0 1 0 0 0 1 0"/>
-        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2124_172"/>
-        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2124_172" result="shape"/>
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2208_1530"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2208_1530" result="shape"/>
       </filter>
     </defs>
   </svg>
-
 </template>
 
 <style scoped>
